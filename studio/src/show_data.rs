98b0165c91c627a92fc1d417f97aa0f653e38c88
danesinoo <carlorosso02@gmail.com>
2023-02-04 20:07:59 +0100

    I completed studio, though it doesn't calculate the average time i spend on one activity each day of the week

--- studio/src/show_data.rs
+++ studio/src/show_data.rs
@@ -5,7 +5,9 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::PathBuf;
+use std::time::Instant;
 
+#[inline]
 pub fn path() -> PathBuf {
     let mut home = home_dir().expect("u windows, ah");
     home.push(".config");
@@ -16,14 +18,11 @@ pub fn path() -> PathBuf {
 }
 
 pub fn display_info() {
-    let mut raw_data = RawData::from_file(&path());
-    raw_data.iter_mut().for_each(|raw| {
-        raw.clean_up();
-    });
+    let now = Instant::now();
+    let raw_data = RawData::from_file(&path());
     raw_data.iter().for_each(|raw_data| {
-        let mut data = DataOnTime::new(raw_data.weekday());
-        let mut last_week = raw_data.get_week(0);
-        last_week.clean_up();
+        let mut data = DataOnTime::new(Local::now().weekday().succ());
+        let last_week = raw_data.week(0);
         data.start_time(&last_week)
             .end_time(&last_week)
             .total_time(&last_week)
@@ -35,8 +34,11 @@ pub fn display_info() {
         table.printstd();
         println!();
     });
+    println!();
+    println!("Time elapsed: {:?}ms", now.elapsed());
 }
 
+/// Memorizza i dati relativi ad una attività
 #[derive(Debug, Clone)]
 struct RawData {
     name: String,
@@ -44,7 +46,18 @@ struct RawData {
     end_time: Vec<DateTime<Local>>,
 }
 
+/// I dati che sono mostrati alla fine
+#[derive(Debug, Clone, Copy)]
+struct DataOnTime {
+    w: Weekday,
+    st: [u16; 7],
+    et: [u16; 7],
+    tt: [u16; 7],
+    at: [u16; 7],
+}
+
 impl RawData {
+    #[inline]
     fn new(name: &str) -> Self {
         RawData {
             name: name.to_string(),
@@ -53,6 +66,7 @@ impl RawData {
         }
     }
 
+    #[inline]
     fn push(&mut self, time: &DateTime<Local>, start: bool) {
         if start && self.start_time.len() == self.end_time.len() {
             self.start_time.push(*time);
@@ -70,17 +84,21 @@ impl RawData {
         }
     }
 
+    #[inline]
     fn parse_time(string: &str) -> DateTime<Local> {
         Local
             .datetime_from_str(string, "%Y-%m-%d %H:%M:%S%.f %:z")
             .unwrap()
     }
 
+    /// Vettore di attività
+    /// i dati in ciascuna attività sono ordinati in base
+    /// al tempo: prima i dati più vecchi
     fn from_file(file: &PathBuf) -> Vec<Self> {
         let file = OpenOptions::new().read(true).open(file).unwrap();
 
         let reader = BufReader::new(file).lines();
-        reader
+        let mut res = reader
             .map(|line| {
                 let line = line.unwrap();
                 let words = line.split_whitespace().collect::<Vec<_>>();
@@ -94,75 +112,76 @@ impl RawData {
             .fold(Vec::new(), |mut acc, (name, flag, time)| {
                 Self::compute_index(&name, flag, &time, &mut acc);
                 acc
-            })
-    }
-
-    fn weekday(&self) -> Weekday {
-        self.end_time.last().unwrap().weekday().succ()
-    }
-
-    // se comincio a studiare un giorno e finisco oltre la mezzanotte, i tempi
-    // di studio vengono suddivisi in due giorni, not yet
-    fn clean_up(&mut self) -> &mut Self {
-        /*
-        println!("{:?}", self);
-        let mut starts = Vec::new();
-        let mut ends = Vec::new();
-        let mut i = 0;
-        while i < self.start_time.len() {
-            if let Some(end) = self.end_time.iter().find(|x| *x > &self.start_time[i]) {
-                starts.push(self.start_time[i]);
-                ends.push(end.clone());
-                if let Some(index) = self.start_time.iter().position(|x| x > end) {
-                    i = index;
-                } else {
-                    break;
-                }
-            } else {
-                break;
+            });
+        res.iter_mut().for_each(|x| {
+            if x.start_time.len() > x.end_time.len() {
+                x.end_time.push(Local::now());
+                x.day();
             }
-        }
-
-        self.start_time = starts;
-        self.end_time = ends;
-        */
-        if self.start_time.len() > self.end_time.len() {
-            self.end_time.push(Local::now());
-        }
-        self.start_time.sort();
-        self.start_time.reverse();
-        self.end_time.sort();
-        self.end_time.reverse();
-        self
+        });
+        res
     }
 
-    fn get_week(&self, which: u16) -> Self {
+    fn day(&mut self) {
+        let mut i = 0;
+        println!("{}", self.name);
+        while i < self.start_time.len() {
+            if self.start_time[i].weekday() != self.end_time[i].weekday() {
+                // spezzo lo studio nei giorni che lo compongono
+                let mut end = self.start_time[i].clone();
+                end = end.with_hour(23).unwrap().with_minute(59).unwrap();
+                let mut start = self.end_time[i].clone();
+                start = start.with_hour(0).unwrap().with_minute(0).unwrap();
+
+                self.end_time.insert(i, end);
+                self.start_time.insert(i + 1, start);
+            }
+            let form = "%H:%M";
+            println!(
+                "{} - {}",
+                self.start_time[i].format(form),
+                self.end_time[i].format(form)
+            );
+            i += 1;
+        }
+    }
+
+    /// prende i dati relativi alla settimana indicata da which:
+    /// 0 -> settimana corrente
+    /// 1 -> settimana scorsa
+    /// ...
+    fn week(&self, which: u16) -> Self {
+        let cond1 = |time: &DateTime<Local>| {
+            (Local::now().num_days_from_ce() - time.num_days_from_ce()) as u16 / 7 == which
+        };
+
+        let cond2 = |x: &DateTime<Local>| !cond1(x);
+
+        // prendo gli indici dei dati che mi interessano
+        let from = self
+            .start_time
+            .iter()
+            .position(cond1)
+            .unwrap_or(self.start_time.len() - 1);
+        let to = self.end_time[from..]
+            .iter()
+            .position(cond2)
+            .unwrap_or(self.end_time.len());
+
+        if from == to {
+            return RawData::new(&self.name);
+        }
+
         RawData {
             name: self.name.clone(),
-            start_time: get_week(&mut self.start_time.clone(), which),
-            end_time: get_week(&mut self.end_time.clone(), which),
+            start_time: self.start_time[from..to].to_vec(),
+            end_time: self.end_time[from..to].to_vec(),
         }
     }
-}
-
-fn get_week(raws: &Vec<DateTime<Local>>, which: u16) -> Vec<DateTime<Local>> {
-    raws.iter()
-        .partition(|x| {
-            (Local::now().num_days_from_ce() - x.num_days_from_ce()) as u16 / 7 < which + 1
-        })
-        .0
-}
-
-#[derive(Debug, Clone, Copy)]
-struct DataOnTime {
-    w: Weekday,
-    st: [u16; 7],
-    et: [u16; 7],
-    tt: [u16; 7],
-    at: [u16; 7],
 }
 
 impl DataOnTime {
+    #[inline]
     fn new(w: Weekday) -> Self {
         DataOnTime {
             w,
@@ -174,57 +193,55 @@ impl DataOnTime {
     }
 
     fn start_time(&mut self, raw: &RawData) -> &mut Self {
+        let mut pos = 0;
         self.st.iter_mut().for_each(|start| {
-            if let Some(data) = raw
-                .start_time
+            if let Some(index) = raw.start_time[pos..]
                 .iter()
-                // sarebbe meglio andare dalla direzione giusta e non dalla fine...
-                .rev()
-                .find(|data| data.weekday() == self.w)
+                .position(|data| data.weekday() == self.w)
             {
-                *start = (data.hour() * 60 + data.minute()) as u16;
+                *start = num_min(&raw.start_time[index + pos]);
+                pos = index + 1;
             } else {
                 *start = 0;
             }
             self.w = self.w.succ();
         });
+        println!();
         self
     }
 
     fn end_time(&mut self, raw: &RawData) -> &mut Self {
-        self.et.iter_mut().for_each(|x| {
-            if let Some(data) = raw.end_time.iter().find(|x| x.weekday() == self.w) {
-                *x = (data.hour() * 60 + data.minute()) as u16;
+        let mut pos = raw.end_time.len();
+        self.et.iter_mut().rev().for_each(|end| {
+            self.w = self.w.pred();
+            if let Some(index) = raw.end_time[..pos]
+                .iter()
+                .rposition(|x| x.weekday() == self.w)
+            {
+                *end = num_min(&raw.end_time[index]);
+                pos = index;
             } else {
-                *x = 0;
+                *end = 0;
             }
-            self.w = self.w.succ();
         });
         self
+    }
+
+    #[inline]
+    fn tt_index(t: &DateTime<Local>, w: Weekday) -> usize {
+        (7 + t.weekday().num_days_from_monday() - w.num_days_from_monday()) as usize % 7
     }
 
     // NB qui c'è un buggino: si chiama il next() e poi break, quindi dopo il
     // primo next, la catena si sfasa e non funziona più
     fn total_time(&mut self, raw: &RawData) -> &mut Self {
-        self.tt.iter_mut().for_each(|tt| {
-            let from = raw
-                .start_time
-                .iter()
-                .position(|x| x.weekday() == self.w)
-                .unwrap_or(raw.end_time.len());
-            let mut to = raw
-                .start_time
-                .iter()
-                .rposition(|x| x.weekday() == self.w)
-                .unwrap_or(0);
-            if to != 0 {
-                to += 1;
-            }
-            *tt = (from..to)
-                .map(|i| (raw.end_time[i] - raw.start_time[i]).num_minutes() as u16)
-                .sum();
-            self.w = self.w.succ();
-        });
+        raw.start_time
+            .iter()
+            .zip(raw.end_time.iter())
+            .for_each(|(st, et)| {
+                let index = Self::tt_index(st, self.w);
+                self.tt[index] += min(et) - min(st);
+            });
         self
     }
 
@@ -242,12 +259,22 @@ impl DataOnTime {
             },
         ))));
 
+        #[inline]
         fn add_row(table: &mut Vec<Row>, data: &[u16], title: &str) {
-            table.push(Row::from(
-                vec![title.to_string()]
-                    .into_iter()
-                    .chain(data.iter().map(|&x| format!("{}:{}", x / 60, x % 60))),
-            ));
+            table.push(Row::from(vec![title.to_string()].into_iter().chain(
+                data.iter().map(|x| {
+                    let hour = x / 60;
+                    let min = x % 60;
+                    let f = |x: u16| {
+                        if x < 10 {
+                            format!("0{}", x)
+                        } else {
+                            format!("{}", x)
+                        }
+                    };
+                    format!("{}:{}", f(hour), f(min))
+                }),
+            )));
         }
 
         add_row(&mut rows, &self.st, "Start");
@@ -256,4 +283,14 @@ impl DataOnTime {
         add_row(&mut rows, &self.at, "Average");
         rows
     }
+}
+
+#[inline]
+fn min(t: &DateTime<Local>) -> u16 {
+    (t.hour() * 60 + t.minute()) as u16
+}
+
+#[inline]
+fn num_min(time: &DateTime<Local>) -> u16 {
+    (time.hour() * 60 + time.minute()) as u16
 }
